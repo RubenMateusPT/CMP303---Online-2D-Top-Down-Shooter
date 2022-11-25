75c63141aa9c4f23a01e3369cf1df8bf19f4e132
Ruben Mateus <rubenmateus.info@gmail.com>
2022-11-25 16:36:59 +0000

    Zara dont touch it

---
+++ Assets/ClientManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ClientManager : MonoBehaviour
+{ 
+	TcpClient tcpClient;
+
+	public void Connect(){
+
+	}
+}
